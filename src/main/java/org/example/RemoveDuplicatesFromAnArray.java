62527e7a57565d082a1602f33342ca3f1c862074
Sibasish9088 <88055760+Sibasish9088@users.noreply.github.com>
2023-07-20 10:52:02 +0530

    Optimized Set and ArrayList implementation for duplicate number removal from Array example

--- src/main/java/org/example/RemoveDuplicatesFromAnArray.java
+++ src/main/java/org/example/RemoveDuplicatesFromAnArray.java
@@ -7,11 +7,9 @@ import java.util.Set;
 public class RemoveDuplicatesFromAnArray {
 
     public static ArrayList<Integer> returnDistinctListOfNumbersFrom(ArrayList<Integer> listOfNumbers) {
-        Set<Integer> distinctSet = new HashSet<>();
-        ArrayList<Integer> distinctList = new ArrayList<>();
-        distinctSet.addAll(listOfNumbers);
+        Set<Integer> distinctSet = new HashSet<>(listOfNumbers);
 
-        distinctList.addAll(distinctSet);
+        ArrayList<Integer> distinctList = new ArrayList<>(distinctSet);
 
         return distinctList;
     }
