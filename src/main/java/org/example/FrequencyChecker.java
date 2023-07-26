a2238981b4f1ce8d0ab811b3e3247519e77c328f
Sibasish9088 <88055760+Sibasish9088@users.noreply.github.com>
2023-07-26 22:00:37 +0530

    Optimized for loop in FrequencyChecker

--- src/main/java/org/example/FrequencyChecker.java
+++ src/main/java/org/example/FrequencyChecker.java
@@ -6,11 +6,11 @@ public class FrequencyChecker {
 
     public static HashMap<Integer, Integer> calculateFrequency(int[] listOfNumbers) {
         HashMap<Integer, Integer> frequencyMap = new HashMap<>();
-        for (int i = 0; i < listOfNumbers.length; i++) {
-            if (frequencyMap.containsKey(listOfNumbers[i]))
-                frequencyMap.put(listOfNumbers[i], frequencyMap.get(listOfNumbers[i])+1);
+        for (int listOfNumber : listOfNumbers) {
+            if (frequencyMap.containsKey(listOfNumber))
+                frequencyMap.put(listOfNumber, frequencyMap.get(listOfNumber) + 1);
             else
-                frequencyMap.put(listOfNumbers[i], 1);
+                frequencyMap.put(listOfNumber, 1);
         }
         return frequencyMap;
     }
